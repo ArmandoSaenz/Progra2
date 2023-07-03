3292092cacbd8a6c570a0170b85e07391e7e78b7
José Armando Sáenz Esqueda <armando.saenz@diractum.com>
2023-07-03 13:10:34 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Msg = $"Nombre del equipo: {Environment.MachineName} {Environment.NewLine}";
+            Msg += $"Usuario: {Environment.UserName} {Environment.NewLine}";
+            Msg += $"Carpeta de ejecución: {Environment.CurrentDirectory} {Environment.NewLine}";
+        }
+    }
+}
